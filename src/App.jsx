cfdf85db7b18a1dbd8a512f395e47a81a8d7ffb4
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-06-25 11:52:07 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import Die from './Die'
+import { nanoid } from 'nanoid'
+import Confetti from "react-confetti"
+
+function App() {
+    const [dice, setDice] = useState(allNewDice());
+    const [tenzies, setTenzies] = useState(false);
+    const [rollCount, setRollCount] = useState(0);
+    
+    useEffect(() => {
+        const allHeld = dice.every((die) => die.isHeld);
+        const firstValue = dice[0].value;
+        const allSameValue = dice.every((die) => die.value === firstValue);
+        if (allHeld && allSameValue) {
+            setTenzies(true);
+        }
+    }, [dice])
+
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        };
+    }
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie());
+        }
+        return newDice;
+    }
+    
+    function rollDice() {
+        if(!tenzies) {
+            setRollCount((prevCount) => prevCount + 1);
+
+            setDice((oldDice) => oldDice.map(die => {
+                return die.isHeld ? 
+                    die :
+                    generateNewDie();
+            }));
+        } else {
+            setTenzies(false);
+            setDice(allNewDice());
+            setRollCount(0);
+        }
+    }
+    
+    function holdDice(id) {
+        setDice((oldDice) => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die;
+        }));
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ));
+    
+    return (
+        <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. 
+            Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button 
+                className="roll-dice" 
+                onClick={rollDice}
+            >
+                {tenzies ? "New Game" : "Roll"}
+            </button>
+            <p className='game-stats'>Dice roll count: {rollCount}</p>
+        </main>
+    )
+}
+
+export default App
---
+++ src/Die.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    };
+
+    const diceOne = (
+        <div className="first-face">
+            <span className="pip"></span>
+        </div>
+    );
+
+    const diceTwo = (
+        <div className="second-face">
+            <span className="pip"></span>
+            <span className="pip"></span>
+        </div>
+    );
+
+    const diceThree = (
+        <div className="third-face">
+            <span className="pip"></span>
+            <span className="pip"></span>
+            <span className="pip"></span>
+        </div>
+    );
+
+    const diceFour = (
+        <div className="fourth-face">
+            <div className="column">
+                <span className="pip"></span>
+                <span className="pip"></span>
+            </div>
+            <div className="column">
+                <span className="pip"></span>
+                <span className="pip"></span>
+            </div>
+        </div>
+    );
+
+    const diceFive = (
+        <div className="fifth-face">
+            <div className="column">
+                <span className="pip"></span>
+                <span className="pip"></span>
+            </div>
+            <div className="column">
+                <span className="pip"></span>
+            </div>
+            <div className="column">
+                <span className="pip"></span>
+                <span className="pip"></span>
+            </div>
+        </div>
+    );
+
+    const diceSix = (
+        <div className="sixth-face">
+            <div className="column">
+                <span className="pip"></span>
+                <span className="pip"></span>
+                <span className="pip"></span>
+            </div>
+            <div className="column">
+                <span className="pip"></span>
+                <span className="pip"></span>
+                <span className="pip"></span>
+            </div>
+        </div>
+    );
+
+    let diceFace;
+    switch (props.value) {
+        case 1:
+            diceFace = diceOne;
+            break;
+        case 2:
+            diceFace = diceTwo;
+            break;
+        case 3:
+            diceFace = diceThree;
+            break;
+        case 4:
+            diceFace = diceFour;
+            break;
+        case 5:
+            diceFace = diceFive;
+            break;
+        case 6:
+            diceFace = diceSix;
+    }
+
+    return (
+        <div 
+            className="die-wrapper"
+            style={styles}
+            onClick={props.holdDice}
+        >
+            {/* <h2 className="die-num">{props.value}</h2> */}
+            {diceFace}
+        </div>
+    )
+}
